106fec36466266081314ab94337ca8e09affea38
sangam <sangamgrg89@gmail.com>
2023-11-15 23:37:18 +1100

    create a dashboard layout with upload, update and delete function

---
+++ mern-client/src/dashboard/EditBooks.jsx
@@ -0,0 +1,200 @@
+import React, { useState } from "react";
+import { useLoaderData, useParams } from "react-router-dom";
+import { Button, Checkbox, Label, TextInput, Textarea } from "flowbite-react";
+
+const EditBooks = () => {
+  const { id } = useParams();
+  const {
+    bookTitle,
+    authorName,
+    imageURL,
+    category,
+    bookDescription,
+    bookPDFURL,
+  } = useLoaderData();
+
+  const bookCategories = [
+    "Fiction",
+    "Non-Fiction",
+    "Mystery",
+    "Romantic",
+    "Science Fiction",
+    "Fantasy",
+    "History",
+    "Horror",
+    "Bibliography",
+    "Autobiography",
+    "Self-help",
+    "Memoir",
+    "Business",
+    "Children",
+    "Travel",
+    "Religion",
+    "Art and Design",
+  ];
+
+  const [selectedBookCategory, setSelectedBookCategory] = useState(
+    bookCategories[0]
+  );
+
+  const handleChangeSelectedValue = (event) => {
+    // console.log(event.target.value);
+    setSelectedBookCategory(event.target.value);
+  };
+
+  //handle book update
+  const handleUpdate = (event) => {
+    event.preventDefault();
+    const form = event.target;
+
+    const bookTitle = form.bookTitle.value;
+    const authorName = form.authorName.value;
+    const imageURL = form.imageURL.value;
+    const category = form.categoryName.value;
+    const bookDescription = form.bookDescription.value;
+    const bookPDFURL = form.bookPDFURL.value;
+
+    const updateBookObj = {
+      bookTitle,
+      authorName,
+      imageURL,
+      category,
+      bookDescription,
+      bookPDFURL,
+    };
+
+    // console.log(bookObj);
+
+    //update book data
+    fetch(`http://localhost:5000/book/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updateBookObj),
+    })
+      .then((res) => res.json)
+      .then((data) => {
+        // console.log(data);
+        alert("Book is updated sucessfully!");
+      });
+  };
+
+  return (
+    <div className="px-4 my-12">
+      <h2 className="mb-8 text-3xl font-bold">Update the book data</h2>
+
+      <form
+        onSubmit={handleUpdate}
+        className="flex lg:w-[1180px] flex-col flex-wrap gap-4">
+        {/* first row*/}
+        <div className="flex gap-8">
+          {/* book title */}
+          <div className="lg:w-1/2">
+            <div className="mb-2 block">
+              <Label htmlFor="bookTitle" value="Book Title" />
+            </div>
+            <TextInput
+              id="bookTitle"
+              name="bookTitle"
+              type="text"
+              placeholder="Book name"
+              required
+              defaultValue={bookTitle}
+            />
+          </div>
+
+          {/* authorname */}
+          <div className="lg:w-1/2">
+            <div className="mb-2 block">
+              <Label htmlFor="authorName" value="Author Name" />
+            </div>
+            <TextInput
+              id="authorName"
+              name="authorName"
+              type="text"
+              placeholder="Author name"
+              required
+              defaultValue={authorName}
+            />
+          </div>
+        </div>
+
+        {/* second row*/}
+        <div className="flex gap-8">
+          {/* book image url */}
+          <div className="lg:w-1/2">
+            <div className="mb-2 block">
+              <Label htmlFor="imageURL" value="Book Image URL" />
+            </div>
+            <TextInput
+              id="imageURL"
+              name="imageURL"
+              type="text"
+              placeholder="Book image URL"
+              required
+              defaultValue={imageURL}
+            />
+          </div>
+
+          {/* category */}
+          <div className="lg:w-1/2">
+            <div className="mb-2 block">
+              <Label htmlFor="inputState" value="Book Category" />
+            </div>
+
+            <select
+              name="categoryName"
+              id="inputState"
+              className="w-full rounded"
+              value={selectedBookCategory}
+              onChange={handleChangeSelectedValue}>
+              {bookCategories.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+
+        {/* book description */}
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="bookDescription" value="Book Description" />
+          </div>
+          <Textarea
+            id="bookDescription"
+            name="bookDescription"
+            placeholder="Add a book description..."
+            required
+            className="w-full"
+            rows={6}
+            defaultValue={bookDescription}
+          />
+        </div>
+
+        {/* book pdf link */}
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="bookPDFURL" value="Book PDF URL" />
+          </div>
+          <TextInput
+            id="bookPDFURL"
+            type="text"
+            name="bookPDFURL"
+            placeholder="book pdf url"
+            required
+            defaultValue={bookPDFURL}
+          />
+        </div>
+
+        <Button type="submit" className="mt-5">
+          Update Book
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default EditBooks;
